574d11c44b9d52df04575b1ac26057568ea8b671
Milan Sebes <milansebes12@gmail.com>
2025-01-15 19:45:08 +0100

    Add initial portfolio files

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Formulier validatie
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault(); // Voorkom standaardverzending
+    alert('Bedankt voor je bericht! We nemen spoedig contact met je op.');
+});
